622b0918aa1fcc562b9c6b4aa4853c36fdea1cc8
donbreo <donbreo@gmail.com>
2021-05-03 17:34:45 +0530

    bash changes

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-pm2 delete jainam_jewellers 2> /dev/null || true
-pm2 kill
 cd src
 yarn
 yarn build
-pm2 start --name "jainam_jewellers" npm -- run start --
+pm2 restart jainam_jewellers || pm2 start --name "jainam_jewellers" npm -- run start --
 pm2 save
