0be45b0c99ef0ca80411aa2fbbddbbc8a7c1b646
donbreo <donbreo@gmail.com>
2021-05-03 17:09:05 +0530

    adding carousel and other styling

--- src/pages/homepage/desktop.js
+++ src/pages/homepage/desktop.js
@@ -2,7 +2,7 @@ import styles from '../../styles/Home.module.css'
 import animate from '../../styles/Animation.module.css'
 import {useEffect} from 'react'
 import PriceWidget from '../components/price/price'
-
+import Carousel from '../components/carousel/desktop'
 export default function Home() {
     
     useEffect(()=>{
@@ -12,10 +12,10 @@ export default function Home() {
     return (
         <div className={styles.homepage_content_wrapper}>
             <div className={styles.body_section}>
-                <div className={`${styles.banner_content} ${animate.fade_in_2sec}` }>
+                <div className={`${styles.banner_content} fade-2s`}>
                     <div className={styles.content_heading}>Finest Jewellery</div>
-                    <div className={styles.sub_heading}>designs that inspire</div>
-                    <div className={styles.sub_heading}>our collection you must admire</div>
+                    <div className={`${styles.sub_heading} fade-5s`}>designs that inspire</div>
+                    <div className={`${styles.sub_heading} fade-10s`}>our collection you must admire</div>
                     <button className={styles.button1}>View now</button>
                 </div>
                 <div className={styles.content_right_pane}>
@@ -23,6 +23,7 @@ export default function Home() {
                 </div>
 
             </div>
+            <Carousel></Carousel>
         </div>
     )
 }
