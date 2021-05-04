c33b620f846ebaf25b79d53c8c0d202dec705a2b
donbreo <donbreo@gmail.com>
2021-05-05 01:24:23 +0530

    sudofied the script

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 sudo su
 cd /var/lib/jenkins/workspace/JainamJewellers/src
 sudo yarn
-yarn build
-pm2 restart jainam_jewellers || pm2 start --name "jainam_jewellers" npm -- run start --
-pm2 save --force
+sudo yarn build
+sudo pm2 restart jainam_jewellers || sudo pm2 start --name "jainam_jewellers" npm -- run start --
+sudo pm2 save --force
