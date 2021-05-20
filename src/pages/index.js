fa787368f4d65c4c526a5c2eb39fd8a8428b3192
donbreo <donbreo@gmail.com>
2021-05-20 13:35:00 +0530

    footer changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import Layout from './components/layout_desktop'
 import Home from './homepage/desktop'
+import HomeMobile from './homepage/desktop'
+import isMobile from 'react-device-detect'
 export default function Index() {
   return (
     <Layout>
+      {isMobile?
+      <HomeMobile>
+
+      </HomeMobile>
+      :
       <Home>
 
-      </Home>
+      </Home>}
     </Layout>
   )
 }
