d3d7e782f7fcd30e8b07871d6b40bd149a4e9c53
donbreo <donbreo@gmail.com>
2021-06-27 18:05:06 +0530

    slow gun

---
+++ middleware/isMobile.js
@@ -0,0 +1,12 @@
+import * as Parser from "ua-parser-js";
+
+export default function isMobile(req) {
+    let userAgent;
+    if (req) {
+        userAgent = Parser(req.headers['user-agent'] || "");
+    } else {
+        userAgent = new Parser().getResult();
+    }
+
+    return userAgent?.device?.type === 'mobile';
+}
