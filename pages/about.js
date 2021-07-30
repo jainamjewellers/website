fbb0be1c0fb914e772408786f7f80ec6397b93d7
donbreo <donbreo@gmail.com>
2021-07-30 16:28:54 +0530

    layout mobile

--- pages/about.js
+++ pages/about.js
@@ -1,31 +1,29 @@
-import Layout from './components/layout'
-import AboutUs from './about/aboutus_desktop'
-import AboutUsMobile from './about/aboutus_mobile'
-import isMobile from '../middleware/isMobile'
+import Layout from "./components/layout";
+import LayoutMobile from './components/layout_mobile'
+import AboutUs from "./about/aboutus_desktop";
+import AboutUsMobile from "./about/aboutus_mobile";
+import isMobile from "../middleware/isMobile";
 
 export default function Index({ isMobile }) {
-    return (
+  return (
+    <>
+      {isMobile ? (
+        <LayoutMobile>
+          <AboutUsMobile></AboutUsMobile>
+        </LayoutMobile>
+      ) : (
         <Layout>
-            {/* <div className="black_bg contactus_bg"><img src="/img/rene-bohmer-YeUVDKZWSZ4-unsplash.jpg"/></div> */}
-
-            {
-                isMobile ?
-                    <AboutUsMobile>
-
-                    </AboutUsMobile>
-                    :
-                    <AboutUs>
-
-                    </AboutUs>
-            }
+          <AboutUs></AboutUs>
         </Layout>
-    )
+      )}
+    </>
+  );
 }
 
 export async function getServerSideProps({ req }) {
-    return {
-        props: {
-            isMobile: isMobile(req),
-        },
-    }
+  return {
+    props: {
+      isMobile: isMobile(req),
+    },
+  };
 }
