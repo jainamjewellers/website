232b9e42a34efcefa6cd635ef7b4ebcdf5bb1e79
donbreo <donbreo@gmail.com>
2021-10-30 00:38:59 +0530

    script changed

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo su
 cd /var/lib/jenkins/workspace/JainamJewellers
+sudo rm -rf ./build
+sudo rm -rf ./node_modules
+sudo rm -rf ./package-lock.json
+sudo rm -rf ./yarn.lock
 sudo yarn
 sudo yarn build
 sudo pm2 restart jainam_jewellers || sudo pm2 start --name "jainam_jewellers" npm -- run start --
